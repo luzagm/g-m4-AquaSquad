1530e9a050f70be31d38a9f200d2f708625cf162
Laura Pareja <lauraparejalmodovar@gmail.com>
2019-09-30 15:38:04 +0200

    Paint UserList

---
+++ src/components/user/ListUserItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const ListUserItem = (props) => {
+    return (
+        <div>Holi</div>
+
+    )
+}
+
+export default ListUserItem;
---
+++ src/components/user/User.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ListUserItem from "./ListUserItem";
+
+
+const UserList = (props) => {
+    return (
+        <ListUserItem />
+    )
+}
+
+export default UserList;
