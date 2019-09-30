8895caec728dfff096d79ebf94fb8a01d550e096
Ana Amaro <amarovazquezana@gmail.com>
2019-09-30 22:18:57 +0200

    add styles to form.scss

--- src/components/user/Form.js
+++ src/components/user/Form.js
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./../Header";
 import Input from "./Inputs";
 // import Button from "../Button";
-import '../../stylesheets/Form.scss'
+import "../../stylesheets/Form.scss";
 
 class Form extends React.Component {
   render() {
@@ -12,14 +12,16 @@ class Form extends React.Component {
         <main className="request__wrap">
           <form className="request__form">
             <h2 className="request__form--title">Solicitud de vacaciones</h2>
-            <div className="left-inputs">
-              <Input label="Desde" placeholder="13/09/2019" />
-              <Input type="select" placeholder="Jefe de proyecto" />
-              <Input type="select" placeholder="Chapter" />
-            </div>
-            <div className="right-inputs">
-              <Input label="Hasta" placeholder="16/09/2019" />
-              <Input type="text" placeholder="Asunto" />
+            <div className="request__form--inputs">
+              <div className="left-inputs">
+                <Input label="Desde" placeholder="13/09/2019" />
+                <Input type="select" placeholder="Jefe de proyecto" />
+                <Input type="select" placeholder="Chapter" />
+              </div>
+              <div className="right-inputs">
+                <Input label="Hasta" placeholder="16/09/2019" />
+                <Input type="text" placeholder="Asunto" />
+              </div>
             </div>
             <button className="request__form--submitButton">Enviar</button>
           </form>
