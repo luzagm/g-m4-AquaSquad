e06f0c72f7b55b06624025f43e9ba5dd05b80828
luzagm <luz.aguirre.mo@gmail.com>
2019-09-30 14:59:52 +0200

    add router

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import "../stylehseets/App.scss";
 
 class App extends React.Component {
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        <main>
+          <Switch>
+            <Route exact path="/" component={Login} />
+            
+            <Route path="/user" component={User List} />
+            <Route path="/user/form" component={Form} />
+            <Route path="/user/confirmation" component={Confirmation} />
+            
+            <Route path="/gestor" component={Gestor List} />
+            <Route path="/gestor/details" component={Gestor Details} />
+            <Route path="/gestor/confirmation" component={Confirmation} />
+          </Switch>
+        </main>
+      </div>
+    );
   }
 }
 
