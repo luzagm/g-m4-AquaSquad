cbcfb069d2ac1ff25c49b5f151c007c762791ec7
saramarin23 <saramarin.econ@gmail.com>
2019-10-01 15:12:12 +0200

    login button center

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,14 +8,16 @@ import "./../stylesheets/components/Login.scss";
 const Login = () => {
   return (
     <React.Fragment>
-      <Header />
+      {/* <Header /> */}
       <div className="login__container">
         <h1 className="login__title">LOGIN</h1>
         <form>
           <Input placeholder="Usuario" />
           <Input placeholder="Contraseña" />
           <Link to="/user/form">
-            <Button name="Acceder" />
+            <div className="login__button--container">
+              <Button name="Acceder" />
+            </div>
           </Link>
         </form>
       </div>
