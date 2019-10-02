2d61469a7193cd60669b29b1921165a3f6d4b571
Laura Pareja <lauraparejalmodovar@gmail.com>
2019-10-02 19:24:44 +0200

    gestor list ready, using json data from holidays and users

--- src/components/gestor/GestorList.js
+++ src/components/gestor/GestorList.js
@@ -7,37 +7,11 @@ import Filters from "../Filters";
 import "../../stylesheets/components/Gestor.scss";
 import "../../stylesheets/App.scss";
 
-// const renderHolidays = dataHolidays => {
-//   return dataHolidays.map(date => {
-//     return date;
-//   });
-// };
-
-// const renderList = (dataResult, dataHolidays) => {
-//   return dataResult.map((user, index) => {
-//     return (
-//       <ListGestorItem
-//         dataResult={user}
-//         dataHolidays={dataHolidays}
-//         key={index}
-//       />
-//     );
-//   });
-// };
 
 const GestorList = props => {
   const { dataResult } = props;
   const holidaysData = dataResult.holidays.holidays;
   const usersData = dataResult.users;
-  console.log(holidaysData);
-  console.log(usersData);
-
-  if (holidaysData.employee_id === usersData.employee_id) {
-    console.log(holidaysData);
-  }
-  // const dataResultDate = dataResult.pop();
-  // const dataHolidays = dataResultDate.holidays;
-  // const holidayPlease = renderHolidays(dataHolidays);
 
   return (
     <div className="gestormain">
@@ -47,7 +21,7 @@ const GestorList = props => {
           <h2 className="mainwrap__title">Gestión de solicitudes</h2>
           {/* <Filters dataResult={dataResult} /> */}
           <ListGestorStatus />
-          <ListGestorItem />
+          <ListGestorItem usersData={usersData} holidaysData={holidaysData} />
         </div>
       </div>
     </div>
--- src/components/gestor/ListGestorItem.js
+++ src/components/gestor/ListGestorItem.js
@@ -2,36 +2,53 @@ import React from "react";
 import Circle from "../Circle";
 import "../../stylesheets/components/ListGestorItem.scss";
 
-//  const renderName = dataResult => {
-//    return dataResult.map(user => {
-//      return <p>{user.name}</p>;
-//    });
-//  };
-
-// const renderDate = dataResult => {
-//   console.log(dataResult.holidays);
-//   return dataResult.holidays.map((date, index) => {
-//     return <p id={index}>{date.date}</p>;
-//   });
-//   //    return dataResult.holidays.map((holiday, key) => {
-//   //    return <p dataResult={holiday.date} key={employee_id}/>;
-//   //  });
-// };
 
 const ListGestorItem = props => {
-  const { dataResult, dataHolidays } = props;
-  console.log(dataHolidays);
+  const { holidaysData, usersData } = props;
+
+  const paintNames = (holidaysData) => {
+    const idFinded = holidaysData.map(holidayUser => {
+      return holidayUser.employee_id
+    })
+    const name = idFinded.map(id => {
+      return usersData.find(user => {
+        return user.employee_id === id
+      })
+    })
+    const listNames = name.map(username => {
+      return <p>{username.name}</p>
+    })
+    return <p>{listNames}</p>
+  }
+
+  const paintProjects = (holidaysData) => {
+    const idFinded = holidaysData.map(holidayUser => {
+      return holidayUser.employee_id
+    })
+    const projects = idFinded.map(id => {
+      return usersData.find(user => {
+        return user.employee_id === id
+      })
+    })
+    const listProjects = projects.map(project => {
+      return <p>{project.project}</p>
+    })
+    return <p>{listProjects}</p>
+  }
+
+  const listHolidaysTotal = holidaysData.map(holiday => {
+    return <p id={holiday.employee_id}>{holiday.date}</p>
+  })
 
   return (
     <div className="mainwrap__gestoritem row col-12">
-      <p className="col-4">{dataHolidays.date}</p>
-
+      <p className="col-4">{listHolidaysTotal}</p>
       <div className="mainwrap__gestoritem--status col-5">
         <Circle />
-        <p>{dataResult.name}</p>
+        <p>{paintNames(holidaysData)}</p>
         {/* <p>{user.chapter_id}</p>  Usaremos este ID para saber qué vacaciones pendiente tiene este usuario */}
       </div>
-      <p className="col-3">{dataResult.project}</p>
+      <p className="col-3">{paintProjects(holidaysData)}</p>
     </div>
   );
 };
