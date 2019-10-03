60a8881b31cad1ea2264a64494c9910c15201f9f
isabel pizarro <chabelipizarro1@gmail.com>
2019-10-03 11:37:58 +0200

    add button accept

--- src/components/gestor/ListGestorItem.js
+++ src/components/gestor/ListGestorItem.js
@@ -13,6 +13,7 @@ const ListGestorItem = props => {
         {/*  <p>{user.chapter_id}</p>  Usaremos este ID para saber qué vacaciones pendiente tiene este usuario*/}
       </div>
       <p className="col-3">Wadus</p>
+      <div className="btn__green">v</div>
     </div>
   );
 };
